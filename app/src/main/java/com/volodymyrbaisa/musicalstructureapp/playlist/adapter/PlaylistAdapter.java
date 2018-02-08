fc9bd85452e069b7bfe6d77ec73779a33654860c
BiosVova <biosvova@gmail.com>
2018-02-07 22:10:11 -0500

    Add labels in AndroidManifest.xml Add images with Multiple-density Changed PlaylistAdapter, was add ViewHolder

--- app/src/main/java/com/volodymyrbaisa/musicalstructureapp/playlist/adapter/PlaylistAdapter.java
+++ app/src/main/java/com/volodymyrbaisa/musicalstructureapp/playlist/adapter/PlaylistAdapter.java
@@ -25,38 +25,44 @@ import butterknife.ButterKnife;
 public class PlaylistAdapter extends ArrayAdapter<PlaylistItem> {
     private int layoutId;
 
-    @BindView(R.id.sounds_title)
-    TextView soundsTitle;
-
-    @BindView(R.id.records)
-    TextView records;
-
-    @BindView(R.id.sounds_time)
-    TextView soundsTime;
-
     public PlaylistAdapter(@NonNull Context context, @LayoutRes int layoutId,
                            @NonNull PlaylistItem[] playlistItems) {
         super(context, 0, playlistItems);
-
         this.layoutId = layoutId;
     }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View root = convertView;
-        if(root == null) {
-         root = LayoutInflater.from(getContext()).inflate(layoutId, parent, false);
+    public View getView(int position, @Nullable View root, @NonNull ViewGroup parent) {
+        ViewHolder holder;
+        if(root != null) {
+            holder = (ViewHolder) root.getTag();
+        } else {
+            root = LayoutInflater.from(getContext()).inflate(layoutId, parent, false);
+            holder = new ViewHolder(root);
+            root.setTag(holder);
         }
-        ButterKnife.bind(this, root);
+
         PlaylistItem playlistItem = getItem(position);
 
         if(playlistItem != null) {
-            soundsTitle.setText(playlistItem.getSoundsTitle());
-            records.setText(playlistItem.getRecords());
-            soundsTime.setText(playlistItem.getSoundsTime());
+            holder.soundsTitle.setText(playlistItem.getSoundsTitle());
+            holder.records.setText(playlistItem.getRecords());
+            holder.soundsTime.setText(playlistItem.getSoundsTime());
         }
 
         return root;
+    }
+
+    static final class ViewHolder {
+        @BindView(R.id.sounds_title) TextView soundsTitle;
+
+        @BindView(R.id.records) TextView records;
+
+        @BindView(R.id.sounds_time) TextView soundsTime;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
     }
 }
